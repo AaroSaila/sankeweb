b90e7695a98c95467582dcdfe5f121493e3799c7
Aaro Saila <aaros.saila@gmail.com>
2025-01-24 22:31:42 +0200

    transformed to spring boot project

---
+++ src/main/java/com/aaros/sankeweb/SankewebApplication.java
@@ -0,0 +1,13 @@
+package com.aaros.sankeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SankewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SankewebApplication.class, args);
+	}
+
+}
