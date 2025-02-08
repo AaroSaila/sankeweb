cb3457c6c8b5fb19eb8143cf6ec5869677532ab8
Aaro Saila <aaros.saila@gmail.com>
2025-02-08 19:01:34 +0200

    typedefs for jsdoc

---
+++ src/main/resources/static/singleplayer/typedefs.js
@@ -0,0 +1,30 @@
+/**
+ * @typedef msg
+ * @type {object}
+ * @property {string} msgType
+ * @property {object} game
+ *
+ * @typedef game
+ * @type {object}
+ * @property {object} board
+ * @property {string} key
+ * @property {number} score
+ * @property {number} tickRate
+ *
+ * @typedef board
+ * @type {object}
+ * @property {object} food
+ * @property {object} sanke
+ *
+ * @typedef food
+ * @type {object}
+ * @property {number} x
+ * @property {number} y
+ *
+ * @typedef sanke
+ * @type {object}
+ * @property {string} dir
+ * @property {array} parts
+ * @property {number} x
+ * @property {number} y
+ */
